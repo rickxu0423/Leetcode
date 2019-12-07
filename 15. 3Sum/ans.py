101a60ac27ec7c9a91ddad7987fc7e446a8a623d
xjy2017cn@hotmail.com <xjy2017cn@hotmail.com>
2019-12-07 17:46:08 -0500

    medium

---
+++ 15. 3Sum/ans.py
@@ -0,0 +1,21 @@
+nums = [-1, 0, 1, 2, -1, -4]
+
+def threeSum(nums):
+    
+    nums = sorted(nums)
+    result = set()
+    for i in range(len(nums)):
+        l = i + 1
+        r = len(nums) - 1
+        target = 0 - nums[i]
+        while l < r:
+            if nums[l] + nums[r] == target:
+                result.add((nums[i], nums[l], nums[r]))
+                l += 1
+                r -= 1
+            elif nums[l] + nums[r] < target:
+                l += 1
+            else:
+                r -= 1
+    return list(result)
+print(threeSum(nums))
