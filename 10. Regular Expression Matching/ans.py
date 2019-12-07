97a1cda4e8528decfaec0c984163aebdaeba0df2
xjy2017cn@hotmail.com <xjy2017cn@hotmail.com>
2019-12-07 17:32:57 -0500

    dp hard

---
+++ 10. Regular Expression Matching/ans.py
@@ -0,0 +1,19 @@
+s = "aab"
+p = "c*a*b"
+
+def isMatch(s, p):
+        memo = {}
+        def dp(i,j):
+            if (i,j) not in memo:
+                if j == len(p):
+                    ans = i ==len(s)
+                else:
+                    match = i < len(s) and p[j] in {s[i],"."}
+                    if j+1 < len(p) and p[j+1] == "*":
+                        ans = dp(i,j+2) or (match and dp(i+1,j))
+                    else:
+                        ans = match and dp(i+1,j+1)
+                memo[i,j] = ans
+            return memo[i,j]
+        return dp(0,0)
+print(isMatch(s,p))
