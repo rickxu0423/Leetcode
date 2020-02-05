c01c2015cb1cac41eeb198152c050c4d42dcd4d9
Jiyun Xu <xjy2017cn@hotmail.com>
2020-02-04 17:39:35 -0800

    bs

---
+++ 35. Search Insert Position/binarysearch.py
@@ -0,0 +1,16 @@
+def searchInsert(nums, target):
+    start, end = 0, len(nums) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return start
+
+nums = [1, 3, 5, 6]
+target = 2
+
+print(searchInsert(nums, target)) # 1
